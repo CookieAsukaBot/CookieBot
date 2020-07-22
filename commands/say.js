7b1bb7100f23f1f460fbd7670a55679b5de23883
Cookie <imhunter_@outlook.com>
2020-07-21 20:07:56 -0700

    Se agregó el comando say

---
+++ commands/say.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'say',
+    description: 'Lo que agregues como mensaje lo diré por ti!',
+    roles: ['admin'],
+    usage: '[mensaje]',
+    execute(message, args) {
+        // Comprobar si tiene args
+        if (!args || args.length <= 0) return message.delete();
+
+        // Mensaje
+        let msg = args.join(' '); // https://stackoverflow.com/questions/28007949/how-to-convert-array-into-string-without-comma-and-separated-by-space-in-javascr/28007965
+
+        // Eliminar mensaje
+        message.delete();
+
+        // Mostrar por consola
+        let showConsole = `[#${message.channel.name}][!say][${message.author.tag}] ${msg}`;
+        console.log(showConsole); // opcional?
+
+        // Enviar mensaje
+        message.channel.send(msg);
+    }
+};
