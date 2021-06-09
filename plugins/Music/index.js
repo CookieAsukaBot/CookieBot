ed9116796db3e43451b6a7c51dd69d6242cae930
Cookie <imhunter_@outlook.com>
2021-06-09 16:10:09 -0700

    se removieron cosas innecesarias de testing

--- plugins/Music/index.js
+++ plugins/Music/index.js
@@ -1,9 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
 module.exports = {
     name: 'Music',
     version: '1.0.0',
-    description: '¡Este plugin te escuchar música!',
-    plugin (bot) {
-        // require('./controller');
-        console.log('yep cock')
+    cookiebot: '1.0.0',
+    description: '¡Plugin oficial de CookieBot para escuchar música!',
+    dependencies: [],
+    async plugin (bot) {
+        // Cargar comandos
+        const commandPath = path.join(__dirname, 'commands');
+        require('../../events/commands')(bot, commandPath);
     }
 };
