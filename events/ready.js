cf17e255294c7502c520f7de613a571f526b5498
RikkaHasCookies <imhunter_@outlook.com>
2022-03-04 05:29:15 -0800

    estados y actividades personalizable

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,13 @@
 module.exports = (bot) => {
-    // Al iniciar el bot
     bot.once('ready', () => {
         console.log(`[BOT] [${process.env.BOT_NAME || "BOT"}] Se ha conectado a Discord.`);
+    });
+
+    bot.user.setPresence({
+        status: process.env.BOT_STATUS || 'online',
+        activities: [{
+            type: process.env.BOT_ACTIVITY_TYPE || "WATCHING",
+            name: process.env.BOT_ACTIVITY || "Cookies"
+        }]
     });
 };
