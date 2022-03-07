9f8137d1251aaf5aca6d064c9e268a4bee4c9e07
RikkaHasCookies <imhunter_@outlook.com>
2022-03-07 11:21:12 -0800

    soporte para rutas y slashCommands

--- events/commands.js
+++ events/commands.js
@@ -1,15 +1,19 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('node:path');
+const fs = require('node:fs');
 
-module.exports = (bot, commandPath) => {
-    // Obtener comandos
-    const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+module.exports = (bot, commandPath, slashCommands) => {
+    const commandFiles = fs.readdirSync(commandPath)
+        .filter(file => file.endsWith('.js'));
 
     // Por cada comando
-    commandFiles.forEach(file => {
+    commandFiles.forEach(async file => {
         const command = require(path.join(commandPath, file));
 
         // Agrega el comando a la Collection
-        bot.commands.set(command.name, command);
+        if (slashCommands) {
+            await bot.slashCommands.set(command.data.name, command);
+        } else {
+            await bot.commands.set(command.name, command);
+        };
     });
 };
