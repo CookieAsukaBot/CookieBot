1f719383a5ec25540b0886098f9a5f10a5f790ae
RikkaHasCookies <imhunter_@outlook.com>
2022-07-04 16:42:23 -0700

    errata, cooldown y menos pings

--- commands/reload.js
+++ commands/reload.js
@@ -1,18 +1,23 @@
 module.exports = {
 	name: 'reload',
-    description: 'Recarga un comando!',
+    description: 'Recarga un comando.',
     roles: ['admin', 'mod'],
-    usage: '[comando]',
+    usage: '<comando>',
+    cooldown: 3,
 	execute (message, args) {
         // Si no hay args
-        if (!args[0]) return message.reply({ content: 'no haz mencionado qué comando recargar!' });
+        if (!args[0]) return message.channel.send({
+            content: `**${message.author.username}**, no has mencionado qué comando recargar!`
+        });
 
         // Buscar comando
         const commandName = args[0].toLowerCase();
         const command = message.client.commands.get(commandName || message.client.find(c => c.alises && c.aliases.includes(commandName)));
 
         // Si el comando no existe
-        if (!command) return message.reply({ content: `no se encontró el comando \`${command.name}\`` });
+        if (!command) return message.channel.send({
+            content: `**${message.author.username}**, no se encontró el comando \`${command.name}\``
+        });
 
         // Cargar comando
         try {
@@ -23,11 +28,13 @@ module.exports = {
             // Agregar al Collection
             message.client.commands.set(newCommand.name, newCommand);
             // Responder
-            message.reply({ content: `Se volvió a cargar el comando \`${commandName}\`!` });
+            message.channel.send({
+                content: `Se volvió a cargar el comando \`${commandName}\`.`
+            });
         } catch (err) {
             console.log(err);
             // Responder
-            message.reply({
+            message.channel.send({
                 content: `Ocurrió un error al volver a cargar el comando \`${commandName}\`.`
             });
         };
