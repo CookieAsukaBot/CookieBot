28243847a3441af198a42648ac97fcfe40359c99
RikkaHasCookies <imhunter_@outlook.com>
2023-04-09 11:42:29 -0700

    updated to latest discord.js v13

--- bot.js
+++ bot.js
@@ -1,24 +1,25 @@
-const { Client, Intents, Collection } = require('discord.js');
+const {Client, Intents, Collection} = require('discord.js');
 const path = require('node:path');
 
 // Cliente
 const bot = new Client({
     intents: [
-        Intents.FLAGS.DIRECT_MESSAGES,
-        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
         Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_BANS,
         Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
         Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
         Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_WEBHOOKS
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING
+        // Intents.FLAGS.MESSAGE_CONTENT
     ]
 });
 
@@ -43,6 +44,6 @@ const events = async () => { // todo: remover await innecesarios?
     await require('./events/interactionCreate')(bot);
     await require('./plugins/index')(bot);
     // require('./events/guildMember')(bot);
-};
+}
 
 events();
