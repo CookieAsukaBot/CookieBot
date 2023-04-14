d88482ad9528275854af9692566175a5a1118282
RikkaHasCookies <imhunter_@outlook.com>
2023-04-14 08:12:59 -0700

    más intents

--- bot.js
+++ bot.js
@@ -6,10 +6,13 @@ const bot = new Client({
     intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildVoiceStates,
 		GatewayIntentBits.MessageContent,
 		GatewayIntentBits.GuildMembers
     ],
     partials: [
+        Partials.User,
         Partials.Message,
         Partials.Channel,
         Partials.Reaction
